444c82d3b18873cfcec3868410e10c83ab7a523f
AngelMtz <20230020@uthh.edu.mx>
2024-06-19 08:11:31 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$nombre = "Angel Uriel";
+$edad = "18";
+$estatura = "1.68";
+
+echo "Me llamo " .$nombre. " tengo " .$edad. " y mido " .$estatura. "<br>";
+echo "<br>";
+
+$x = 10;
+$y = 5;
+
+echo "La suma de $x  +  $y = "  .$x+$y. " <br>";
+echo "La resta de $x  -   $y  = " .$x-$y. "<br>";
+echo "La multiplicacion de $x  *  $y = " .$x*$y. "<br>";
+echo "La division de  $x / $y  = " .$x/$y. "<br>";
+echo "La exponenciacion de  $x elevado a  $y = " .$x**$y. "<br>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <br>
+    Hola Mundo
+</body>
+</html>
